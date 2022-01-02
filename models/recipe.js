1d1f6951c174895b17278c0ddb042ae79fc10ae1
Tokismos <tokismos@gmail.com>
2022-01-02 16:49:52 +0100

    set false to some champs

--- models/recipe.js
+++ models/recipe.js
@@ -35,7 +35,7 @@ const recipeSchema = new mongoose.Schema({
   },
   tempsAttente: {
     type: Number,
-    required: false,
+    required: true,
   },
   tempsPreparation: {
     type: Number,
@@ -43,7 +43,7 @@ const recipeSchema = new mongoose.Schema({
   },
   tempsCuisson: {
     type: Number,
-    required: false,
+    required: true,
   },
   material: {
     type: Array,

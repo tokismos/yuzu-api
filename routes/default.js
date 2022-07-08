455b2c72374b98f816ae3074e66bc4ba2a6db282
edjubert <edouard.jubert@gmail.com>
2022-07-08 15:23:45 +0200

    chore: normal default route

--- routes/default.js
+++ routes/default.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const router = express.Router()
 
-const { sendMail } = require('./email');
-
-router.get("/", async (req, res) => {
-  console.log('Calling sendMail');
-  await sendMail(req, res);
-  res.status(200).send({ msg: 'hello' })
-});
+router.get("/", async (_, res) => res.status(200).send({ msg: 'hello' }));
 
 module.exports = router;

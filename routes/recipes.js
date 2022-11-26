cd24951f6f0f0ed591b89e6186144dbf2cc49c4d
Hugo Gautier <gautier.hgo@gmail.com>
2022-11-26 14:37:41 +0100

    test img compression

--- routes/recipes.js
+++ routes/recipes.js
@@ -266,20 +266,20 @@ router.post("/editImg", async (req, res) => {
 
   try{
     
-
-    const result = await recipe.find({ imgUrl: req.body.oldImg });
+    res.status(200).send({data:req.body.oldImg});
+    // const result = await recipe.find({ imgUrl: req.body.oldImg });
   
-    await recipe.findByIdAndUpdate(
-        result[0]._id,
-        {imgUrl : req.body.newImg},
-        function(err, result) {
-          if (err) {
-            res.send(err);
-          } else {
-            res.status(200).send(result);
-          }
-        }
-        )
+    // await recipe.findByIdAndUpdate(
+    //     result[0]._id,
+    //     {imgUrl : req.body.newImg},
+    //     function(err, result) {
+    //       if (err) {
+    //         res.send(err);
+    //       } else {
+    //         res.status(200).send(result);
+    //       }
+    //     }
+    //     )
   }
   catch(err) {
     res.status(400).send({ message: "Error, NOT ADDED TO DB", error: err });
